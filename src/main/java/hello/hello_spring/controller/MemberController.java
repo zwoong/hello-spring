0df53f748dc6dfe3e6a8e1f659c9caae146b6db3
zwoong <zwoong6867@gmail.com>
2025-04-28 21:33:40 +0900

    ✨ 회원 웹 기능 - 조회

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -2,8 +2,10 @@ package hello.hello_spring.controller;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -31,5 +33,12 @@ public class MemberController {
 
     memberService.join(member);
     return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model) {
+    List<Member> members = memberService.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
   }
 }
